0a6d68de5aa9ce968491a785555c321d60b9c37b
Sebastian <srugeles99748@umanizales.edu.co>
2023-09-24 12:34:42 -0500

    Initial commit

---
+++ src/main/java/com/linkedlist/tads_api/TadsApiApplication.java
@@ -0,0 +1,13 @@
+package com.linkedlist.tads_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TadsApiApplication.class, args);
+	}
+
+}
